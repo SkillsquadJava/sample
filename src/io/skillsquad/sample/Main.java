a79c86aeae63b7e75459758887f83fa26bc91af7
Prasad Kancharla <kancharlaguruprasad@gmail.com>
2023-07-08 15:40:03 +0530

    Add src directory

---
+++ src/io/skillsquad/sample/Main.java
@@ -0,0 +1,10 @@
+package io.skillsquad.sample;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student(1,"Ram", 5);
+        System.out.println("Name of the student is: " + student.getName());
+    }
+}
